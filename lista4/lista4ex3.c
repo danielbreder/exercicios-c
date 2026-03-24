26ce5e2d125d7a717595ceb14261c081413a61da
danielbreder <129018507+danielbreder@users.noreply.github.com>
2026-03-24 12:15:33 -0300

    lista4 ex3 redone

--- lista4/lista4ex3.c
+++ lista4/lista4ex3.c
@@ -1,42 +1,25 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <locale.h>
 
-char* divisivel(int x, int y){
-    if(x % y == 0){
-        return("O primeiro parametro eh divisivel pelo segundo");
+int ehDivisivel(int a, int b){
+    if(a % b == 0){
+        return 1;
     }
-    else{
-        return "Eles nao sao divisiveis";
-    }
+    return 0;
 }
 
-int main (){
-    int x, y;
+int main() {
+    int x;
 
-    printf("Digite dois numeros: ");
-    scanf("%d %d", &x, &y);
+    printf("Digite um numero: ");
+    scanf("%d", &x);
 
-    if(x%2 == 0 && y%2 != 0){
-        printf("%s\n", divisivel(x, y));
-
-        printf("x eh par e y eh impar");
-    }
-    else if(x%2 != 0 && y%2 == 0){
-        printf("%s\n", divisivel(x, y));
-
-        printf("x eh impar e y eh par");
-    }
-    else if(x%2 == 0 && y%2 == 0){
-        printf("%s\n", divisivel(x, y));
-
-        printf("x eh par e y eh par");
+    if(ehDivisivel(x, 2)){
+        printf("Esse numero eh par");
     }
     else{
-        printf("%s\n", divisivel(x, y));
-
-        printf("x eh impar e y eh impar");
+        printf("Esse numero eh impar");
     }
 
     return 0;
---
+++ lista4/lista4ex3v2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+char* divisivel(int x, int y){
+    if(x % y == 0){
+        return("O primeiro parametro eh divisivel pelo segundo");
+    }
+    else{
+        return "Eles nao sao divisiveis";
+    }
+}
+
+int main (){
+    int x, y;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &x, &y);
+
+    if(x%2 == 0 && y%2 != 0){
+        printf("%s\n", divisivel(x, y));
+
+        printf("x eh par e y eh impar");
+    }
+    else if(x%2 != 0 && y%2 == 0){
+        printf("%s\n", divisivel(x, y));
+
+        printf("x eh impar e y eh par");
+    }
+    else if(x%2 == 0 && y%2 == 0){
+        printf("%s\n", divisivel(x, y));
+
+        printf("x eh par e y eh par");
+    }
+    else{
+        printf("%s\n", divisivel(x, y));
+
+        printf("x eh impar e y eh impar");
+    }
+
+    return 0;
+}
