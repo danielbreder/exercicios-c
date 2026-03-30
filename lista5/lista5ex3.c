faf353c552e420788e4fe73ca4da0de798eb64a5
danielbreder <129018507+danielbreder@users.noreply.github.com>
2026-03-30 19:47:32 -0300

    Create lista5ex3.c

---
+++ lista5/lista5ex3.c
@@ -0,0 +1,23 @@
+int ehPrimo(float n) {
+    int primo = 0;
+    while(n%2!=0){
+
+    }
+    return primo;
+}
+
+main() {
+    float n;
+    
+    printf("Digite um numero: ");
+    scanf("%f", &n);
+
+    if(ehPrimo){
+        printf("Esse numero eh primo");
+    }
+    else {
+        printf("Esse numero nao eh primo");
+    }
+
+    return 0;
+}
